679eb724c40399301d8a2b2c4319f9a58ac5bc60
errborges <116311765+errborges@users.noreply.github.com>
2022-10-21 10:47:13 -0300

    Update instance.route.js

--- src/api/routes/instance.route.js
+++ src/api/routes/instance.route.js
@@ -10,7 +10,7 @@ router.route('/qr').get(keyVerify, controller.qr)
 router.route('/qrbase64').get(keyVerify, controller.qrbase64)
 router.route('/info').get(keyVerify, controller.info)
 router.route('/restore').get(tokenVerify, controller.restore)
-router.route('/logout').delete(keyVerify, loginVerify, controller.logout)
+router.route('/logout').get(keyVerify, loginVerify, controller.logout)
 router.route('/delete').delete(keyVerify, loginVerify, controller.delete)
 router.route('/list').get(tokenVerify, controller.list)
 
